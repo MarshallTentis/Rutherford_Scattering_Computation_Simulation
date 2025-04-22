caf28e3fc5707e590c5b64333b2b4e44d6f41134
Sean CHen <Seanchendu@gmail.com>
2025-04-21 22:53:16 -0500

    guide

---
+++ function.py
@@ -0,0 +1,24 @@
+
+def get_x():
+  return 0
+
+def get_y():
+  return 0
+
+def get_Vx():
+  return 0
+
+def get_Vy():
+  return 0
+
+def get_Ax():
+  return 0
+
+def get_Ay():
+  return 0
+
+def get_Fx():
+  return 0
+
+def get_Fy():
+  return 0
